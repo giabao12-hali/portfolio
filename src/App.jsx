186e413dd5afb1b1ecde63ac99ad197ac433c252
Gia Bao <nguyenhiengiabao12@gmail.com>
2024-09-29 16:59:07 +0700

    Refactor UI code for better readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -148,9 +148,10 @@ function App() {
                 <div className="timeline-start mb-10 md:text-end">
                   <time className="font-mono italic">Jun 2023 ~ August 2023</time>
                   <div className="text-lg font-black">.NET Developer | Personal ASP.NET Blazor E-Commerce Website Project</div>
-                  A personal project to develop an E-Commerce website using ASP.NET Blazor. The project aims to provide a platform for users to purchase products and manage their accounts. The website will be designed with a user-friendly interface and optimized for performance and security to ensure a smooth shopping experience for users.
-                  <div className='flex justify-end mt-4'>
-                    <a className='link' href='https://github.com/giabao12-hali/BlazorEcommerce' target='_blank'>
+                  A personal project to develop an E-Commerce website using <span className='font-bold'>ASP.NET Blazor</span>. The project aims to provide a platform for users to purchase products and manage their accounts. The website will be designed with a user-friendly interface and optimized for performance and security to ensure a smooth shopping experience for users.
+                  <div className='flex flex-col justify-end mt-4 gap-2'>
+                    <p className='font-bold'>Source Code:</p>
+                    <a className='link flex justify-end' href='https://github.com/giabao12-hali/BlazorEcommerce' target='_blank'>
                       <Github />
                     </a>
                   </div>
@@ -174,11 +175,21 @@ function App() {
                 <div className="timeline-end mb-10">
                   <time className="font-mono italic">September 2023 ~ December 2023</time>
                   <div className="text-lg font-black">Front-End Developer | Graduation Donace Project</div>
-                  Graduation project website focuses on developing an online event ticketing system. Built primarily with Next.js, Tailwind CSS, and NextUI for UI design, the site offers a modern and user-friendly experience. Users can easily browse and purchase tickets for events, manage their ticket information, and complete transactions quickly and conveniently. The responsive interface ensures compatibility across a range of devices, from desktops to mobile phones, providing a seamless experience for all users.
-                  <div className='flex justify-start mt-4'>
-                    <a className='link' href='https://github.com/Donace-Project/Donace-FE-Production' target='_blank'>
-                      <Github />
-                    </a>
+                  Graduation project website focuses on developing an online event ticketing system. Built primarily with <span className='font-bold'>Next.js, Tailwind CSS, and NextUI</span> for UI design, the site offers a modern and user-friendly experience. Users can easily browse and purchase tickets for events, manage their ticket information, and complete transactions quickly and conveniently. The responsive interface ensures compatibility across a range of devices, from desktops to mobile phones, providing a seamless experience for all users.
+                  <div className='flex flex-col justify-start mt-4 gap-1'>
+                    <div>
+                      <p className='font-bold py-2'>Website:</p>
+                      <p className='cursor-default font-semibold'>
+                        Click&nbsp;
+                        <a className='link tooltip tooltip-right' data-tip="🙋‍♂️" href='https://donace-fe-production.vercel.app/' target='_blank'>here</a>
+                      </p>
+                    </div>
+                    <div>
+                      <p className='font-bold py-2'>Source Code:</p>
+                      <a className='link' href='https://github.com/Donace-Project/Donace-FE-Production' target='_blank'>
+                        <Github />
+                      </a>
+                    </div>
                   </div>
                 </div>
                 <hr />
@@ -200,7 +211,23 @@ function App() {
                 <div className="timeline-start mb-10 md:text-end">
                   <time className="font-mono italic">January 2024 ~ February 2024</time>
                   <div className="text-lg font-black">Freelancer Front-End Developer | Metruyenchu</div>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque laudantium laboriosam molestias, culpa odio consequatur? Modi harum odio ut officia iusto sint quae, architecto, provident sapiente at est hic quis!
+                  A novel reading platform built using <span className='font-bold'>ReactJS</span> for the frontend and <span className='font-bold'>DaisyUI</span> for component-based styling, offering a responsive and user-friendly interface. The website ensures seamless navigation and enhances the reading experience with a modern, minimalist design.
+                  <div className='flex flex-col justify-end mt-4 gap-2'>
+                    <div>
+                      <p className='font-bold py-2'>Website:</p>
+                      <p className='cursor-default font-semibold'>
+                        Click&nbsp;
+                        <a className='link tooltip' data-tip="hi there, it's me again" href='https://story-reading.vercel.app/' target='_blank'>here</a>
+                        &nbsp;👈
+                      </p>
+                    </div>
+                    <div>
+                      <p className='font-bold py-2'>Source Code:</p>
+                      <a className='link flex justify-end' href='https://github.com/TranDat1114/Story-Reading' target='_blank'>
+                        <Github />
+                      </a>
+                    </div>
+                  </div>
                 </div>
                 <hr />
               </li>
@@ -220,8 +247,24 @@ function App() {
                 </div>
                 <div className="timeline-end mb-10">
                   <time className="font-mono italic">May 2023 ~ May 2024</time>
-                  <div className="text-lg font-black">.NET Developer | HQSOFT - Distribution & Retail Expert </div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore possimus ipsam ducimus repellat esse ullam nobis unde asperiores, quae sequi, ad praesentium ex similique minus in soluta fugit id quod.
+                  <div className="text-lg font-black">Intership .NET Developer | HQSOFT - Distribution & Retail Expert </div>
+                  The ERP (Enterprise Resource Planning) Software is built using <span className='font-bold'>.NET 8</span> technology, incorporating Blazor for component-based web development, and utilizing DevExpress, MudBlazor, and Bootstrap for dynamic and responsive user interfaces. This ERP system efficiently handles business operations like finance, supply chain, and human resources through a modern and scalable cloud-based architecture.
+                  <div className='flex flex-col justify-start mt-4 gap-1'>
+                    <div>
+                      <p className='italic py-2'>*At HQSoft, I worked on ERP-related software, but it was developed for internal use, so there’s no link available, hehe. However, I do have the source code from when I practiced coding with the ABP framework to meet the company’s needs:</p>
+                      <p className='cursor-default font-semibold items-center flex'>
+                        Click&nbsp;
+                        <a className='link tooltip tooltip-bottom' data-tip="ohh you found me ^^" href='https://github.com/giabao12-hali/ABPFramework_Pratice' target='_blank'>here</a>
+                        &nbsp;
+                        <label className="swap swap-flip text-base" >
+                          <input type="checkbox" />
+
+                          <div className="swap-on tooltip" data-tip="yes, this way">👈</div>
+                          <div className="swap-off tooltip" data-tip="ops, wrong way">👉</div>
+                        </label>
+                      </p>
+                    </div>
+                  </div>
                 </div>
                 <hr />
               </li>
@@ -240,10 +283,48 @@ function App() {
                   </svg>
                 </div>
                 <div className="timeline-start mb-10 md:text-end">
-                  <time className="font-mono italic">June 2024 ~ ...</time>
-                  <div className="text-lg font-black">The Journey Begins</div>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ea magnam voluptatem assumenda qui accusantium soluta ipsum nobis! Voluptas vero earum nisi vel eaque hic explicabo facilis quae! Facere, voluptatum.
+                  <time className="font-mono italic">August 2024 ~ Present</time>
+                  <div className="text-lg font-black">Learning Flutter & Dart</div>
+                  Learning Flutter and Dart involves mastering a modern framework and programming language for building cross-platform mobile applications. Flutter offers a fast, efficient development process with a rich set of pre-designed widgets, while Dart is a clean, object-oriented language that enables high-performance, responsive UI development. Together, they allow for creating beautiful and scalable apps for iOS, Android, and beyond from a single codebase.
+                  <div className='flex flex-col justify-end mt-4 gap-2'>
+                    <div>
+                      <p className='italic py-2'>*Because I am still continuing to learn and get familiar with the Flutter interface and the Dart language, you can follow my progress on developing apps here:</p>
+                      <p className='cursor-default font-semibold items-center flex justify-end'>
+                        Flutter is so much&nbsp;
+                        <a className='link tooltip tooltip-bottom' data-tip="ohh you found me again" href='https://github.com/giabao12-hali/flutter-project-collection' target='_blank'>fun</a>
+                        &nbsp;
+                        <label className="swap swap-flip text-base" >
+                          <input type="checkbox" />
+
+                          <div className="swap-off tooltip" data-tip="this time">👈</div>
+                          <div className="swap-on tooltip" data-tip="no wrong way anymore">👉</div>
+                        </label>
+                      </p>
+                    </div>
+                  </div>
                 </div>
+                <hr />
+              </li>
+              <li>
+                <hr />
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5">
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd" />
+                  </svg>
+                </div>
+                <div className="timeline-end mb-10">
+                  <time className="font-mono italic">August 2024 ~ Present</time>
+                  <div className="text-lg font-black">Fullstack Web Developer | Vietravel</div>
+                  The Passport/ID Card reading system is built with <span className='font-bold'>.NET 8</span> as the backend and <span className='font-bold'>ReactJS, TailwindCSS, and DaisyUI</span> for the frontend. This system allows the sales team to upload passport or ID card images, automatically extracting customer information such as full name, nationality, passport number, and other relevant details. The extracted data is then stored in the eTour system for further processing and management, streamlining the customer data entry process and improving efficiency in handling bookings.
+                </div>
+
               </li>
             </ul>
           </div>
